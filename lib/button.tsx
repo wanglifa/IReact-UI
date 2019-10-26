1d4d0283312440b54637568f42609cab67d46138
wanglifa <wanglifa1995@gmail.com>
2019-10-26 16:24:33 +0800

    安装了react和react-dom

---
+++ lib/button.tsx
@@ -0,0 +1,8 @@
+function Button() {
+    return (
+        <div>
+            按钮
+        </div>
+    )
+}
+export default Button
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,4 @@
-console.log('小改改')
+import React from 'react';
+import ReactDom from 'react-dom';
+console.log(React)
+console.log(ReactDom)
