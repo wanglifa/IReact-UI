b6f0d79d7958059c348f4803865e6f37330b1bdf
wanglifa <wanglifa1995@gmail.com>
2019-10-30 11:05:30 +0800

    icon 接受 onClick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './importicon'
 import './icon.scss'
 interface IconProps {
-    name: string
+    name: string;
+    onClick: React.MouseEventHandler
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className="ireact-icon">
+        <svg className="ireact-icon" onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`}/>
         </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Icon from './icon'
+let fn = (e: React.MouseEvent) => {
+    console.log((e.target as HTMLDivElement).style)
+}
 ReactDom.render(
     <div>
-       <Icon name="wechat"/>
-       <Icon name="qq"/>
+       <Icon name="qq" onClick={fn}/>
     </div>
     , document.querySelector('#root'));
