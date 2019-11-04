0fae4cdf2e7bf292d1176790042665e99509e352
wanglifa <wanglifa1995@gmail.com>
2019-11-04 16:30:20 +0800

    export default icon and auto publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon'
-let fn = (e: React.MouseEvent) => {
-    console.log(e.target)
-}
-ReactDom.render(
-    <div>
-       <Icon name="qq"
-             onClick={fn}
-             onMouseEnter={() => console.log('mousenter')}
-             onMouseLeave={() => console.log('mouseleave')}
-       />
-    </div>
-    , document.querySelector('#root'));
+export { default as Icon } from './icon/icon'
