beb0b157b93d259bb2f5d9ea95dc2943a6cbefe9
wanglifa <wanglifa1995@gmail.com>
2019-11-21 21:02:58 +0800

    scopedClassMaker 代码扩展优化

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -5,3 +5,25 @@ function classes(...names: (string | undefined)[]) {
 }
 
 export default classes;
+
+interface Options {
+  extra: string | undefined
+}
+
+interface ClassToggles {
+  [K: string]: boolean
+}
+
+const scopedClassMaker = (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : {[name]: name})
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ')
+
+export {scopedClassMaker}
