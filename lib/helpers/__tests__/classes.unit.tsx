26f54bff038ab466a37c90cd313003bd5ecf6c91
wanglifa <wanglifa1995@gmail.com>
2019-11-26 16:09:57 +0800

    classes 添加 单元测试

--- lib/helpers/__tests__/classes.unit.tsx
+++ lib/helpers/__tests__/classes.unit.tsx
@@ -1,4 +1,4 @@
-import classes from '../classes'
+import classes, {scopedClassMaker} from '../classes';
 describe('classes', () => {
     it('接受 1 个className', () => {
         const result = classes('a')
@@ -16,4 +16,15 @@ describe('classes', () => {
         const result = classes()
         expect(result).toEqual('')
     })
+})
+
+describe('scopedClassMaker', () => {
+  it('接受字符串或对象', () => {
+    const sc = scopedClassMaker('ireact-layout')
+    expect(sc('')).toEqual('ireact-layout')
+    expect(sc('x')).toEqual('ireact-layout-x')
+    expect(sc({y: true, z: false})).toEqual('ireact-layout-y')
+    expect(sc({y: true, z: true})).toEqual('ireact-layout-y ireact-layout-z')
+    expect(sc({y: true, z: true}, {extra: 'red'})).toEqual('ireact-layout-y ireact-layout-z red')
+  })
 })
