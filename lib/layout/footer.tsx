7a253ca2daf6c843df11c79f02d80c19a6e7d68f
wanglifa <wanglifa1995@gmail.com>
2019-11-29 13:58:19 +0800

    完善layout footer

--- lib/layout/footer.tsx
+++ lib/layout/footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import {scopedClassMaker} from '../helpers/classes';
 const sc = scopedClassMaker('ireact-layout')
-const Footer: React.FunctionComponent = () => {
+interface Props extends React.HTMLAttributes<HTMLElement>{
+
+}
+
+const Footer: React.FunctionComponent<Props> = (props) => {
+  const {className, ...rest} = props
   return (
-    <div className={sc('footer')}>footer</div>
+    <div className={sc('footer', {extra: className})} {...rest}>
+      {props.children}
+    </div>
   )
 }
 
