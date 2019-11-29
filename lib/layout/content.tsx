0b93d76592d843f7f0a3ed1450580e01c168297c
wanglifa <wanglifa1995@gmail.com>
2019-11-29 14:03:57 +0800

    完善layout content

--- lib/layout/content.tsx
+++ lib/layout/content.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import {scopedClassMaker} from '../helpers/classes';
 const sc = scopedClassMaker('ireact-layout')
-const Content: React.FunctionComponent = () => {
+interface Props extends React.HTMLAttributes<HTMLElement>{
+
+}
+const Content: React.FunctionComponent<Props> = (props) => {
+  const { className, ...rest } = props
   return (
-    <div className={sc('content')}>content</div>
+    <div className={sc('content', {extra: className})} {...rest}>
+      {props.children}
+    </div>
   )
 }
 
