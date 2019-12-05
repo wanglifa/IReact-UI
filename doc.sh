f06c153fba43bf6c099dd22156d00818eefe387a
wanglifa <wanglifa1995@gmail.com>
2019-12-05 14:00:50 +0800

    modify doc.sh

--- doc.sh
+++ doc.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 yarn doc
 git checkout gh-pages
-mv -f dco/* ./
+mv -f doc/* ./
 git add .
-git commit -m 'update'
-git push origin gh-pages
-git checkout master
+git commit -m "update"
+git push
+git checkout -
