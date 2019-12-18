40f30271b43808bc4927dcd93b4d8c1f70df57a1
wanglifa <wanglifa1995@gmail.com>
2019-12-18 11:22:28 +0800

    简单轮播（未实现真正的无缝滚动）

--- lib/slides/slides.example.tsx
+++ lib/slides/slides.example.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import Slides from "./slides";
 const SlidesExample: React.FunctionComponent = () => {
   return (
-    <Slides>
+    <Slides width={400}>
       <div className="box">1</div>
-      <div className="box">2</div>
+      <div className="box" style={{background: "red"}}>2</div>
       <div className="box">3</div>
-      <div className="box">4</div>
+      <div className="box" style={{background: "blue"}}>4</div>
     </Slides>
   )
 }
--- lib/slides/slides.tsx
+++ lib/slides/slides.tsx
@@ -1,66 +1,96 @@
 import * as React from "react";
 import {scopedClassMaker} from '../helpers/classes';
-import {ReactElement, useEffect, useState, Fragment} from "react";
-const scopedClass = scopedClassMaker('ireact-slides')
-import { CSSTransition } from 'react-transition-group';
-const sc = scopedClass
 import './slides.scss'
-interface Prop {
-  children: ReactElement[];
-  visible?: boolean;
+import {Fragment, ReactElement, useEffect, useRef, useState} from "react";
+const scopedClass = scopedClassMaker('ireact-slides')
+const sc = scopedClass
+interface Props extends React.HTMLAttributes<ReactElement>{
+  width: number;
+  children: Array<ReactElement>;
   duration?: number;
 }
-interface cloneParams {
-  visible: boolean
+interface cloneParams extends React.HTMLAttributes<ReactElement>{
+
 }
-const Slides: React.FunctionComponent<Prop> = (props) => {
+const Slides: React.FunctionComponent<Props> = (props) => {
+  let [current, setCurrent] = useState(0)
+  let timer: number = 0
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const childrenLength = props.children.length
   const cloneElement = (child: ReactElement, cloneParams: cloneParams): ReactElement => {
     return React.cloneElement(child, cloneParams)
   }
-  let arr: Array<cloneParams> = []
-  let index: number = 0
-  const run = (): void => {
-    index += 1
-    if (index > props.children.length - 1) {
+  const gotToSlide = (index: number): void => {
+    if(index < 0) {
+      index = childrenLength - 1
+    } else if (index > childrenLength - 1) {
       index = 0
     }
-    arr = arr.map((item, index1): cloneParams => {
-      item.visible = index === index1;
-      return item
-    })
-    setState([...arr])
-    timer()
+    if (current === 0 && index === childrenLength - 1) {
+      console.log('从最后一张到第一张')
+      wrapperRef.current!.addEventListener('transitionend', () => {
+        wrapperRef.current!.hidden = true
+        wrapperRef.current!.hidden = false
+      }, { once: true })
+    } else if (current === childrenLength - 1 && index === 0) {
+      console.log('从第一张到最后一张')
+      wrapperRef.current!.addEventListener('transitionend', () => {
+        wrapperRef.current!.classList.add('ireact-hidden')
+        wrapperRef.current!.classList.remove('ireact-hidden')
+      }, { once: true })
+    }
+    setCurrent(n => index)
   }
-  const timer = (): void => {
-    setTimeout(() => {
-      run()
+  const wrapperWidth = (props.children.length + 2) * props.width
+  const makeFakeSlides = () => {
+    const currentDom: HTMLElement = wrapperRef.current!
+    const first = currentDom.children[0].cloneNode(true)
+    const last = wrapperRef.current!.children[currentDom.children.length - 1].cloneNode(true)
+    currentDom.append(first)
+    currentDom.prepend(last)
+    wrapperRef.current!.style.transform = `translateX(${-props.width}px)`
+  }
+  const initTimer = () => {
+    timer = window.setTimeout(() => {
+      gotToSlide(current + 1)
     }, props.duration)
   }
-  let [state, setState] = useState<Array<cloneParams>>(arr)
-  for (let i = 0; i < props.children.length; i++) {
-    arr.push({ visible: false })
-    cloneElement(props.children[i], {visible: state[index].visible})
+  const onMouseEnter = () => {
+    window.clearTimeout(timer)
+  }
+  const onMouseLeave = () => {
+    initTimer()
+  }
+  const onClickDot = (index: number): void => {
+    setCurrent(n => index)
   }
   useEffect(() => {
-    arr[0].visible = true
-    setState([...arr])
-    timer()
-  },[])
+    makeFakeSlides()
+  }, [])
+  useEffect(() => {
+    initTimer()
+  }, [current])
   return (
-    <div className={sc('')}>
+    <div className={sc('')} style={{width: `${props.width}px`}} onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <div className={sc('window')}>
-        <div className={sc('wrapper')}>
-          {
-            state.length === props.children.length ?
-              props.children.map((node, index) =>
-                <Fragment key={index}>
-                    <CSSTransition in={state[index].visible} classNames="fade" timeout={200} unmountOnExit>
-                      <div className={sc('item')} key={index}>{node}</div>
-                    </CSSTransition>
-                </Fragment>
-              )
-              : null
-          }
+        <div className={sc('wrapper')} style={{ width: `${wrapperWidth}px`,
+          transform: `translateX(${-(current * (props.width) + props.width)}px)`}}
+          ref={wrapperRef}
+        >
+          {props.children.map((node: ReactElement, index: number) =>
+            <Fragment key={index}>
+              {cloneElement(node, {style: { width: `${props.width}px`}})}
+            </Fragment>
+          )}
+        </div>
+        <div className={sc('btn-wrapper')}>
+          {props.children.map((node, index1) =>
+            <span className={sc({'dot': true, 'active': current === index1})} key={index1}
+              onClick={() => {onClickDot(index1)}}
+            ></span>
+          )}
         </div>
       </div>
     </div>
