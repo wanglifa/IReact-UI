5b31f166369f827d5746bfa6ce2fa1bdcf19fee5
wanglifa <wanglifa1995@gmail.com>
2020-01-08 14:08:20 +0800

    menu 横向导航实现

--- lib/menu/menu.example.tsx
+++ lib/menu/menu.example.tsx
@@ -5,15 +5,14 @@ import Icon from "../icon/icon";
 const onClick = (val: any) => {
   console.log(val)
 }
-console.log(Menu)
 const MenuExample: React.FunctionComponent = () => {
   return (
-    <Menu onClick={onClick} selectedKeys={["mail"]} mode="horizontal">
-      <Menu.Item key="mail">
+    <Menu onClick={onClick} selectedName={"mail"} mode="horizontal">
+      <Menu.Item name="mail">
         <Icon name={"left"} />
         Navigation One
       </Menu.Item>
-      <Menu.Item key="app" disabled>
+      <Menu.Item name="app" disabled>
         <Icon name={"minus"} />
         Navigation Two
       </Menu.Item>
@@ -26,15 +25,15 @@ const MenuExample: React.FunctionComponent = () => {
         }
       >
         <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
+          <Menu.Item name="setting:1">Option 1</Menu.Item>
+          <Menu.Item name="setting:2">Option 2</Menu.Item>
         </Menu.ItemGroup>
         <Menu.ItemGroup title="Item 2">
-          <Menu.Item key="setting:3">Option 3</Menu.Item>
-          <Menu.Item key="setting:4">Option 4</Menu.Item>
+          <Menu.Item name="setting:3">Option 3</Menu.Item>
+          <Menu.Item name="setting:4">Option 4</Menu.Item>
         </Menu.ItemGroup>
       </SubMenu>
-      <Menu.Item key="alipay">
+      <Menu.Item name="alipay">
         <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
           Navigation Four - Link
         </a>
--- lib/menu/menu.tsx
+++ lib/menu/menu.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
-import {ReactElement} from "react";
+import {createContext, Fragment, ReactElement, useContext, useEffect, useRef, useState} from "react";
+import {scopedClassMaker} from '../helpers/classes';
+const scopedClass = scopedClassMaker('ireact-menu')
+import './menu.scss'
+const sc = scopedClass
+const C = createContext<any | null>(null)
 interface menuProp {
   onClick?: (val: any) => void;
-  selectedKeys: string[];
+  selectedName: string;
   mode?: 'vertical' | 'horizontal';
+  children: Array<ReactElement>;
 }
 interface ItemProp {
-  key: string;
+  name: string;
   disabled?: boolean;
+  mode1?: 'vertical' | 'horizontal';
 }
 interface SubMenuProp {
-  key?: string;
+  name?: string;
   title: ReactElement;
+  mode1?: 'vertical' | 'horizontal';
+  children: Array<ReactElement>;
 }
 interface ItemGroupProp {
-  key?: string;
+  name?: string;
   title: string;
+  mode1?: 'vertical' | 'horizontal';
+  children: Array<ReactElement>;
+}
+interface cloneParams {
+  mode1: string
 }
 interface Prop extends React.FunctionComponent<menuProp>{
   Item: React.FunctionComponent<ItemProp>;
@@ -23,24 +37,98 @@ interface Prop extends React.FunctionComponent<menuProp>{
   ItemGroup: React.FunctionComponent<ItemGroupProp>;
 }
 const Menu: Prop = (prop) => {
+  const cloneElement = (child: ReactElement, cloneParams: cloneParams): ReactElement => {
+    return React.cloneElement(child, cloneParams)
+  }
+  const [selectName, setSelectName] = useState<string>('')
+  useEffect(() => {
+    setSelectName(prop.selectedName)
+  }, [])
   return (
-    <div>{prop.children}</div>
+    <C.Provider value={{ selectName, setSelectName, onClick: prop.onClick }}>
+      <div className={sc({'': true, 'horizontal': prop.mode === 'horizontal'})}>
+        {prop.children!.map((node: ReactElement, index: number) =>
+          <Fragment key={index}>
+            {cloneElement(node, {mode1: prop.mode!})}
+          </Fragment>
+        )}
+      </div>
+    </C.Provider>
   )
 }
-const Item: React.FunctionComponent = (prop) => {
+const Item: React.FunctionComponent<ItemProp> = (prop) => {
+  const {selectName, setSelectName, onClick} = useContext(C)
+  const onClickItem = (val: string) => {
+    onClick(val)
+    setSelectName(val)
+  }
   return (
-    <div>{prop.children}</div>
+    <Fragment>
+      <div className={sc({'item': true, 'item-disabled': prop.disabled ? true : false,
+        'item-selected': selectName === prop.name
+      })}
+        onClick={() => onClickItem(prop.name)}
+      >
+        {prop.children}
+      </div>
+    </Fragment>
   )
 }
-const SubMenu: React.FunctionComponent = (prop) => {
+const SubMenu: React.FunctionComponent<SubMenuProp> = (prop) => {
+  const [popMinWidth, setPopMinWidth] = useState<number>(0)
+  const titleRef = useRef<HTMLDivElement | null>(null)
+  const [navVisible, setNavVisible] = useState<boolean>(false)
+  const {selectName} = useContext(C)
+  const [SubMenuLists, setSubMenuLists] = useState<string[]>([])
+  const arr: string[] = []
+  const deepGetItemList = (obj: any) => {
+    if (typeof obj.children === 'string') {
+      arr.push(obj.name)
+    } else {
+      obj.children.map((node: ReactElement) => {
+        deepGetItemList(node.props)
+      })
+    }
+  }
+  useEffect(() => {
+    deepGetItemList(prop)
+    setSubMenuLists(arr)
+    const { width } = titleRef.current!.getBoundingClientRect()
+    setPopMinWidth(width)
+  }, [])
+  useEffect(() => {
+    SubMenuLists.includes(selectName) ? setNavVisible(false) : null
+  }, [selectName])
   return (
-    <div>{prop.children}</div>
+    <Fragment>
+      <div className={sc({'submenu': true, [`submenu-${prop.mode1}`]: true, 'item-selected': SubMenuLists.includes(selectName)})}
+        onMouseEnter={() => setNavVisible(true)}
+           onMouseLeave={() => setNavVisible(false)}
+      >
+        {
+          prop.title ?
+            <div className={sc('submenu-title')} ref={titleRef}>
+              {prop.title}
+            </div> : null
+        }
+        <div className={sc('submenu-popup')} style={{display: navVisible ? 'block' : 'none'}}>
+          <div className={sc({'': true, 'vertical': true})} style={{minWidth: `${popMinWidth}px`}}>{prop.children}</div>
+        </div>
+      </div>
+    </Fragment>
   )
 }
-const ItemGroup: React.FunctionComponent = (prop) => {
+const ItemGroup: React.FunctionComponent<ItemGroupProp> = (prop) => {
   return (
-    <div>
-      {prop.children}
+    <div className={sc('item-group')}>
+      {
+        prop.title ?
+          <div className={sc('item-group-title')}>{prop.title}</div>
+          : null
+      }
+      <div className={sc('item-group-list')}>
+        {prop.children}
+      </div>
     </div>
   )
 }
