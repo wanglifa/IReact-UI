6e810de4dd54bf106fc822823de93f817d5ab2f9
wanglifa <wanglifa1995@gmail.com>
2020-02-19 15:27:57 +0800

    隐藏浏览器滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
-const Scroll: React.FunctionComponent = () => {
+import {HTMLAttributes} from "react";
+import './scroll.scss'
+interface Prop extends HTMLAttributes<HTMLElement>{
+
+}
+const Scroll: React.FunctionComponent<Prop> = (props) => {
+  const {children, ...rest} = props
   return (
-    <div>scroll</div>
+    <div {...rest} className={"ireact-scroll"}>
+      <div className="ireact-scroll-inner">
+        {children}
+      </div>
+    </div>
   )
 }
 export default Scroll
